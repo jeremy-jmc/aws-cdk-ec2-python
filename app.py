479798cdad6170af5f19a2b852100ccf3a035ae3
JerM1609 <jeremy.matos@utec.edu.pe>
2024-09-15 20:27:28 -0500

    mods

--- app.py
+++ app.py
@@ -2,6 +2,7 @@
 import os
 
 import aws_cdk as cdk
+from aws_cdk import DefaultStackSynthesizer
 
 from project.project_stack import ProjectStack
 
@@ -11,11 +12,32 @@ ProjectStack(app, "ProjectStack",
     # If you don't specify 'env', this stack will be environment-agnostic.
     # Account/Region-dependent features and context lookups will not work,
     # but a single synthesized template can be deployed anywhere.
+    synthesizer = DefaultStackSynthesizer(
+        bootstrap_stack_version_ssm_parameter="/cdk-bootstrap/${Qualifier}/version",
+        bucket_prefix="",
+        
+        cloud_formation_execution_role="arn:${AWS::Partition}:iam::${AWS::AccountId}:role/LabRole",
+        
+        deploy_role_arn="arn:${AWS::Partition}:iam::${AWS::AccountId}:role/LabRole",
+        deploy_role_external_id="",
+        
+        generate_bootstrap_version_rule=True,
+        
+        file_asset_publishing_role_arn="arn:${AWS::Partition}:iam::${AWS::AccountId}:role/LabRole",
+        file_asset_publishing_external_id="",
+        file_assets_bucket_name="cdk-${Qualifier}-assets-${AWS::AccountId}-${AWS::Region}",
+        
+        image_asset_publishing_role_arn="arn:${AWS::Partition}:iam::${AWS::AccountId}:role/LabRole",
+        image_asset_publishing_external_id="",
+        image_assets_repository_name="cdk-${Qualifier}-container-assets-${AWS::AccountId}-${AWS::Region}",
 
+        lookup_role_arn="arn:${AWS::Partition}:iam::${AWS::AccountId}:role/LabRole",
+        lookup_role_external_id="",
+    ),
     # Uncomment the next line to specialize this stack for the AWS Account
     # and Region that are implied by the current CLI configuration.
 
-    #env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
+    env=cdk.Environment(account=os.getenv('CDK_DEFAULT_ACCOUNT'), region=os.getenv('CDK_DEFAULT_REGION')),
 
     # Uncomment the next line if you know exactly what Account and Region you
     # want to deploy the stack to. */
--- project/project_stack.py
+++ project/project_stack.py
@@ -1,7 +1,10 @@
+import aws_cdk as core
 from aws_cdk import (
     # Duration,
     Stack,
     # aws_sqs as sqs,
+    aws_ec2 as ec2,
+    aws_iam as iam,
 )
 from constructs import Construct
 
@@ -11,9 +14,79 @@ class ProjectStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        
+        instance_name = self.node.try_get_context("InstanceName") or "MV Reemplazar"
+        ami_id = self.node.try_get_context("AMI") or "ami-0aa28dab1f2852040"
+        role = self.format_arn(service='iam', region='', account=self.account, resource='role/LabRole')
+        
+        # Security Group
+        security_group = ec2.SecurityGroup(
+            self, "InstanceSecurityGroup",
+            vpc=ec2.Vpc.from_lookup(self, "MyDefaultVPC", is_default=True),
+            description="Permitir trafico SSH y HTTP desde cualquier lugar",
+            allow_all_outbound=True
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "ProjectQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        security_group.add_ingress_rule(
+            peer=ec2.Peer.any_ipv4(),
+            connection=ec2.Port.tcp(22),
+            description="Permitir SSH"
+        )
+
+        security_group.add_ingress_rule(
+            peer=ec2.Peer.any_ipv4(),
+            connection=ec2.Port.tcp(80),
+            description="Permitir HTTP"
+        )
+
+        # EC2 Instance
+        instance = ec2.Instance(
+            self, "EC2Instance",
+            instance_type=ec2.InstanceType("t2.micro"),
+            machine_image=ec2.MachineImage.generic_linux({
+                "us-east-1": ami_id
+            }),
+            vpc=ec2.Vpc.from_lookup(self, "DefaultVPC", is_default=True),
+            security_group=security_group,
+            key_name="vockey",  # Asegúrate de tener esta llave en tu cuenta de AWS
+            block_devices=[
+                ec2.BlockDevice(
+                    device_name="/dev/sda1",
+                    volume=ec2.BlockDeviceVolume.ebs(20)
+                )
+            ],
+            role=iam.Role.from_role_arn(self, "InstanceRole", role)
+        )
+
+        instance.add_user_data(
+            "#!/bin/bash",
+            "cd /var/www/html/",
+            "git clone https://github.com/utec-cc-2024-2-test/websimple.git",
+            "git clone https://github.com/utec-cc-2024-2-test/webplantilla.git",
+            "ls -l"
+        )
+
+        # Outputs
+        core.CfnOutput(
+            self, "InstanceId",
+            value=instance.instance_id,
+            description="ID de la instancia EC2"
+        )
+
+        core.CfnOutput(
+            self, "InstancePublicIP",
+            value=instance.instance_public_ip,
+            description="IP pública de la instancia"
+        )
+
+        core.CfnOutput(
+            self, "websimpleURL",
+            value=f"http://{instance.instance_public_ip}/websimple",
+            description="URL de websimple"
+        )
+
+        core.CfnOutput(
+            self, "webplantillaURL",
+            value=f"http://{instance.instance_public_ip}/webplantilla",
+            description="URL de webplantilla"
+        )
